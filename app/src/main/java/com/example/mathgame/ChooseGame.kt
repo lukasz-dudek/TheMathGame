352bdaa9538003990d1b3144a41c178121441c92
lukasz-dudek <ldudek@qtf.io>
2023-06-02 14:53:07 +0200

    choose game screen changes + public variable userName

--- app/src/main/java/com/example/mathgame/ChooseGame.kt
+++ app/src/main/java/com/example/mathgame/ChooseGame.kt
@@ -4,14 +4,22 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import kotlin.system.exitProcess
 
 class ChooseGame : AppCompatActivity() {
 
+    override fun onBackPressed() {
+        finishAffinity()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_game)
+
+        findViewById<TextView>(R.id.welcome_user_text_view).text = getString(
+            R.string.choose_game_screen_welcome_user, MainActivity.etUserName.text)
 
         val btnAdding : Button = findViewById(R.id.btn_adding)
         btnAdding.setOnClickListener {
--- app/src/main/java/com/example/mathgame/MainActivity.kt
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mathgame
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,12 +10,18 @@ import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        @SuppressLint("StaticFieldLeak")
+        lateinit var etUserName: EditText
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
-        val etUserName : EditText = findViewById(R.id.et_user_name)
+        etUserName = findViewById(R.id.et_user_name)
         val btnStart : Button = findViewById(R.id.btn_start)
         btnStart.setOnClickListener {
 
